f1e5a696bdb95863991e0d6d617a7315d183beb7
Peter Mescalchin <peter@magnetikonline.com>
2018-08-29 23:37:28 +1000

    Run scripts now create a tmpfs at /dev/disk/by-id to keep server startup happier

--- run-tmpfs.sh
+++ run-tmpfs.sh
@@ -9,5 +9,6 @@ STATE_TMPFS_SIZE_MB="4096"
 docker run \
 	--publish "$SERVER_HTTP_PORT:$SERVER_HTTP_PORT" \
 	--rm \
+	--tmpfs "/dev/disk/by-id:noexec,rw,size=4k" \
 	--tmpfs "$STATE_DIR:noexec,rw,size=${STATE_TMPFS_SIZE_MB}m" \
 		"$DOCKER_IMAGE_NAME"
--- run.sh
+++ run.sh
@@ -7,4 +7,5 @@ SERVER_HTTP_PORT="6878"
 docker run \
 	--publish "$SERVER_HTTP_PORT:$SERVER_HTTP_PORT" \
 	--rm \
+	--tmpfs "/dev/disk/by-id:noexec,rw,size=4k" \
 		"$DOCKER_IMAGE_NAME"
